0469659868b6d62004e8bb242c49cca5d04a1740
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2025-07-26 03:23:51 +0200

    tick with scheduled functions

--- convex/crons.ts
+++ convex/crons.ts
@@ -4,10 +4,12 @@ import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
-crons.interval(
-  "tickGames",
-  { seconds: 1 }, // every minute
-  internal.games.tickGames,
-);
+// NOTE: This in old way of doing the scheduling
+// see end of `startGame` in `games.ts` (the evaluation whats better is still open)
+// crons.interval(
+//   "tickGames",
+//   { seconds: 1 }, // every minute
+//   internal.games.tickGames,
+// );
 
 export default crons;
