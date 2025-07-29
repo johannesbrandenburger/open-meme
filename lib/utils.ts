16582ca1df131bfaa2aa5b593dc1d45220331b95
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2025-07-29 03:02:04 +0200

    fix voting loading state

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { makeUseQueryWithStatus } from "convex-helpers/react";
+import { useQueries } from "convex/react";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const useQueryWithStatus = makeUseQueryWithStatus(useQueries);
